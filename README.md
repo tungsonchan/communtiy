e2a38ce7a6f3f676c47d1091694664bd1a61e6d1
tungsonchan <tungsonchan@qq.com>
2020-01-13 21:57:44 +0800

    add check cookies

--- README.md
+++ README.md
@@ -10,8 +10,14 @@
 
 [Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
 
+[Spring Boot Features](https://docs.spring.io/spring-boot/docs/2.2.2.RELEASE/reference/html/spring-boot-features.html#boot-features-sql)
+
+[mvnrepository](https://mvnrepository.com/)
+
 ## 工具
 
 [Git](https://git-scm.com/download)
 
 [VP](https://www.visual-paradigm.com)
+
+[mybatis-spring-boot-autoconfigure](https://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/)
--- src/main/java/issc/zcdong/community/controller/IndexController.java
+++ src/main/java/issc/zcdong/community/controller/IndexController.java
@@ -1,15 +1,36 @@
 package issc.zcdong.community.controller;
 
+import issc.zcdong.community.mapper.UserMapper;
+import issc.zcdong.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String hello(){
+    public String index(HttpServletRequest request){
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.fingByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
