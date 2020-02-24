91feabc9b9cae04146ffd1564e7d65387becf8c9
tungsonchan <tungsonchan@qq.com>
2020-02-24 21:14:44 +0800

    修复MYSQL

--- src/main/resources/db/migration/V8__Change_creator_column_type.sql
+++ src/main/resources/db/migration/V8__Change_creator_column_type.sql
@@ -1,2 +1,2 @@
-alter table question modify creator bigint not null auto_increment;
-alter table comment modify commentator bigint not null auto_increment;
+alter table question modify creator bigint not null;
+alter table comment modify commentator bigint not null;
